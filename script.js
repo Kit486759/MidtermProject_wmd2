d256e12f71ea18629bbab6b2bdfedc42712600ca
kit486759 <kit486759@gmail.com>
2021-03-08 12:24:36 -0800

    first day

---
+++ script.js
@@ -0,0 +1,8 @@
+const api = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+
+const searchbox = document.getElementsByClassName("search-box");
+searchbox.addEventListener("click", searchWeather)
+
+function searchWeather (){
+
+}
