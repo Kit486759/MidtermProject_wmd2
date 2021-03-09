f09340b381c929236011ed4d1ce04e1c1d737c6d
kit486759 <kit486759@gmail.com>
2021-03-08 20:39:23 -0800

    -a

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+// API
 const api = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
 
+
+// Target the search bar
 const searchbox = document.getElementsByClassName("search-box");
 searchbox.addEventListener("click", searchWeather)
 
+
+// My function for searching weather
 function searchWeather (){
 
 }
