e64c8ef3f0c12577e5be8a8aac6d48ee5c913466
kit486759 <kit486759@gmail.com>
2021-03-09 23:55:02 -0800

    21Mar09 update

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
-// API
-const api = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+
+// let default city = Vancouver 
+let city = "vancouver"
+// let cityinput from search 
+let cityInput = `${city}`
 
 
-// Target the search bar
-const searchbox = document.getElementsByClassName("search-box");
-searchbox.addEventListener("click", searchWeather)
+// API  
+const APIkey = "bb38ad245b56243dda6f74c97a8bcf6c"
+const api = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`
+// const api = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${APIkey}`
 
 
-// My function for searching weather
-function searchWeather (){
+// Testing search button and search box - 21mar09
+var searchBtn = document.getElementById("searchBtn")
+var searchInput = document.getElementById("searchInput")
 
+// fetch 
+fetch(api)
+
+    .then(response => {
+        if (response.status == 200) {
+            // alert("connect success");
+            response.json().then(result => {
+                console.log(result);
+                console.log(result.name);
+            })
+        }
+    })
+
+
+// Detect the click action on "search button"
+searchBtn.addEventListener("click", () => {
+    searchWeather();
+    alert("test search button");
+
+})
+
+// Function for searching weather TESTING
+function searchWeather() {
+    document.getElementById("city").InnerHTML = (`<p>hihi</p>`)
 }
+
